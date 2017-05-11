c0b676f413d79ebb0037ebd0a145a4ce19c70ff9
Ziad Ahmed <z.ahmed@live.com>
2017-05-11 19:36:36 +0000

    exercise 7 works and prints proper error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,9 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+@new_store_name = gets.chomp
+@new_store = Store.create()
+puts @new_store.errors.full_messages
+#binding.pry
+
+
